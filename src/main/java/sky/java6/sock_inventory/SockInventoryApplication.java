22728aa6751f6bd057f39df8457d0175a4951085
ruslan--t <ruslan--t@yandex.ru>
2023-12-05 23:03:26 +0400

    1

---
+++ src/main/java/sky/java6/sock_inventory/SockInventoryApplication.java
@@ -0,0 +1,13 @@
+package sky.java6.sock_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SockInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sky/java6/sock_inventory/SockInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.java6.sock_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SockInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
