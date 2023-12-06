128d16b6f2125934ab0856a77b407fbf96b557fa
ruslan--t <ruslan--t@yandex.ru>
2023-12-06 23:07:03 +0400

    get-Запрос работает

--- src/main/java/sky/java6/sock_inventory/service/SockServiceImpl.java
+++ src/main/java/sky/java6/sock_inventory/service/SockServiceImpl.java
@@ -12,14 +12,15 @@ import sky.java6.sock_inventory.repository.SocksRepository;
 
 @Component
 @RequiredArgsConstructor
-public class SockServiceImpl implements SockService{
+public class SockServiceImpl implements SockService {
     private final SocksRepository repository;
+
     @Override
     public Integer getReport(String color, Operation operation, Integer cottonPart) {
 
-        if (operation == Operation.moreThan) {
+        if (operation == Operation.valueOf(String.valueOf(Operation.moreThan))) {
             return repository.getQuantityByColorAndCottonPartMoreThen(color, cottonPart);
-        } else if (operation == Operation.lessThan) {
+        } else if (operation == Operation.valueOf(String.valueOf(Operation.lessThan))) {
             return repository.getQuantityByColorAndCottonPartLessThen(color, cottonPart);
         } else {
             return repository.getQuantityByColorAndCottonPartEqualsThen(color, cottonPart);
@@ -29,11 +30,11 @@ public class SockServiceImpl implements SockService{
     @Override
     public void income(NewMoving newMoving) {
         Socks socks = repository.findByColorAndCottonPart(newMoving.getColor(), newMoving.getCottonPart());
-        if(newMoving.getQuantity() < 1 || newMoving.getCottonPart() > 100){
+        if (newMoving.getQuantity() < 1 || newMoving.getCottonPart() > 100) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        }else if(socks == null){
+        } else if (socks == null) {
             repository.save(NewMoving.toSocks(newMoving));
-        }else{
+        } else {
             socks.setQuantity(socks.getQuantity() + newMoving.getQuantity());
             repository.save(socks);
         }
@@ -42,9 +43,9 @@ public class SockServiceImpl implements SockService{
     @Override
     public void outcome(NewMoving newMoving) {
         Socks socks = repository.findByColorAndCottonPart(newMoving.getColor(), newMoving.getCottonPart());
-        if(socks == null || newMoving.getQuantity() > socks.getQuantity()){
+        if (socks == null || newMoving.getQuantity() > socks.getQuantity()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        }else{
+        } else {
             socks.setQuantity(socks.getQuantity() - newMoving.getQuantity());
             repository.save(socks);
         }
